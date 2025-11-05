7c0570eefd358318bf054db1beedaf8e915ad020
KrukYash <181584157+KrukYash@users.noreply.github.com>
2025-11-05 12:27:26 +0530

    Add files via upload

---
+++ SmilingFace.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class SmilingFace extends Frame {
+
+    public SmilingFace() {
+        setTitle("Smiling Face");
+        setSize(400, 400);
+        setVisible(true);
+        
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public void paint(Graphics g) { 
+        setBackground(Color.CYAN);
+        g.setColor(new Color(255, 182, 193)); 
+        g.fillOval(100, 100, 200, 200); 
+        g.setColor(Color.BLACK);
+        g.fillOval(150, 150, 20, 20); 
+        g.fillOval(220, 150, 20, 20); 
+        g.setColor(Color.BLACK);
+        g.drawArc(150, 200, 100, 50, 0, -180); 
+        g.setColor(Color.BLACK);
+        g.drawLine(200, 220, 200, 190);
+    }
+
+    public static void main(String[] args) {   
+        new SmilingFace(); 
+    }
+}
